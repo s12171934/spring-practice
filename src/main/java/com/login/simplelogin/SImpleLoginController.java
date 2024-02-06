a873a167fb7a6b31ae1a814b8e0ef89a50ff8e24
s12171934 <ssong99999@naver.com>
2024-02-06 15:25:42 +0900

    login

--- src/main/java/com/login/simplelogin/SImpleLoginController.java
+++ src/main/java/com/login/simplelogin/SImpleLoginController.java
@@ -1,7 +1,11 @@
 package com.login.simplelogin;
 
+import com.login.simplelogin.dto.LoginForm;
 import com.login.simplelogin.dto.Member;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import jakarta.websocket.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +20,25 @@ public class SImpleLoginController {
     static HashMap<String,Member> memberList = new HashMap<>();
 
     @GetMapping("")
-    public String LoginForm(){
+    public String loginForm(){
         return "redirect:/login.html";
     }
     @GetMapping("/signup")
     public String signUpForm(){
         return "redirect:/signup.html";
+    }
+    @PostMapping("")
+    public String login(@Valid LoginForm loginForm, Errors errors, HttpServletRequest request){
+        boolean isFail = errors.hasErrors()
+                || !memberList.containsKey(loginForm.getEmail())
+                || !memberList.get(loginForm.getEmail()).getPassword().equals(loginForm.getPassword());
+        if(isFail){
+            return "redirect:/login-fail.html";
+        } else{
+            HttpSession session = request.getSession();
+            session.setAttribute("member",memberList.get(loginForm.getEmail()));
+            return "redirect:/after-login.html";
+        }
     }
 
     @PostMapping("/signup")
@@ -34,7 +51,7 @@ public class SImpleLoginController {
             return "redirect:/login/signup";
         } else{
             memberList.put(member.getEmail(),member);
-            return "redirect:/login";
+            return "redirect:/signup-success.html";
         }
     }
 }
---
+++ src/main/java/com/login/simplelogin/dto/LoginForm.java
@@ -0,0 +1,16 @@
+package com.login.simplelogin.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class LoginForm {
+    @NotEmpty
+    @Email
+    String email;
+    @NotEmpty
+    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z]).{8,}")
+    String password;
+}
